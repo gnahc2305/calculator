8cc6eff07ed753629fe0093bf231756bee7ae831
gnahc2305 <achangmurillo@gmail.com>
2022-09-19 17:52:06 -0500

    create functions to do basic math

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('hola');
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return substract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+    }
+}
+
+// console.log(operate('/', 10, 2));
