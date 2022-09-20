faab68b51f56f5213b54fc05ffdb4d729d4fd13c
gnahc2305 <achangmurillo@gmail.com>
2022-09-20 15:12:21 -0500

    Create variables for all the divs in the calculator

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const display_div = document.querySelector('.display');
+const reset_div = document.querySelector('.reset');
+const positiveOrNegative_div = document.querySelector('.positiveOrNegative');
+const percentage_div = document.querySelector('.percentage');
+const division_div = document.querySelector('.division');
+const seven_div = document.querySelector('.seven');
+const eight_div = document.querySelector('.eight');
+const nine_div = document.querySelector('.nine');
+const multiply_div = document.querySelector('.multiply');
+const four_div = document.querySelector('.four');
+const five_div = document.querySelector('.five');
+const six_div = document.querySelector('.six');
+const subtract_div = document.querySelector('.subtract');
+const one_div = document.querySelector('.one');
+const two_div = document.querySelector('.two');
+const three_div = document.querySelector('.three');
+const add_div = document.querySelector('.add');
+const zero_div = document.querySelector('.zero');
+const decimal_div = document.querySelector('.decimal');
+const equals_div = document.querySelector('.equals');
+
+
 function add(a, b) {
     return a + b;
 }
